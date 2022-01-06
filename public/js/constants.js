3856e6fff9d6204b2f25daa2751d6feb2f4747cd
iluvpy <miomanns265@gmail.com>
2022-01-06 20:48:30 +0100

    refactor

--- public/js/constants.js
+++ public/js/constants.js
@@ -24,8 +24,7 @@ const CANVAS_HEIGHT = 720;
 const RESPAWN_TIME = 3000; // it takes 3 seconds to respawn
 const BACKGROUND_COLOR = "#4f555c";
 const RESPAWN_PROT_TIME = 3000; // you are invincible for 3000
-const GAME_MAP_WIDTH = 30000;
-const GAME_MAP_HEIGHT = 30000;
+
 export {
     MAX_NAME_SIZE,
     UPDATE_RATE,
@@ -44,7 +43,5 @@ export {
     PLAYER_FRICTION,
     WEAPONS,
     RESPAWN_TIME,
-    RESPAWN_PROT_TIME,
-    GAME_MAP_HEIGHT,
-    GAME_MAP_WIDTH
+    RESPAWN_PROT_TIME
 };
--- public/js/events.js
+++ public/js/events.js
@@ -5,7 +5,6 @@ export default function html_events() {
     const weapon_image = document.getElementById("weapon");
     
     weapon_image.addEventListener("click", (ev) => {
-        console.log("image was clicked!");
         const src = weapon_image.src;
         var i = 0;
         for (var weapon_src of WEAPONS) {
@@ -14,11 +13,9 @@ export default function html_events() {
         }
         console.log(`i: ${i}`);
         if (i < WEAPONS.length-1) {
-            console.log("set image to next");
             weapon_image.src = WEAPONS[i+1];
         } 
         else {
-            console.log("set image to start");
             weapon_image.src = WEAPONS[0];
         }
     });
